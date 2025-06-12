d86b9876f62ccbd842ad838431a8a254e3af96ae
Surajit Rana <surajit1602@gmail.com>
2025-06-13 00:22:19 +0530

    feat[p1-solid-principles][p1-srp][example-1]: single responsibility principle

--- P1_SOLID_Principles/P1_SRP_Single_Responsibility_Principle/Example_1/Refactored_Code/reporter/persistence/IPersistence.java
+++
@@ -1,5 +0,0 @@
-package P1_SOLID_Principles.P1_SRP_Single_Responsibility_Principle.Example_1.Refactored_Code.reporter.persistence;
-
-public interface IPersistence {
-    public void save(String name);
-}
