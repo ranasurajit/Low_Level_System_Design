e9cd711a50e403e986ee2504a88799e3e567a65d
Surajit Rana <surajit1602@gmail.com>
2025-06-27 22:28:18 +0530

    feat[design-patterns][behavioral-design-patterns][java][p2-strategy-design-pattern][example-2]: strategy design pattern - shopping cart system

--- P4_Behavioral_Design_Patterns/P2_Java_Example_2_Shopping_Cart_System/Refactored_Code/Cart.java
+++ P4_Behavioral_Design_Patterns/P2_Java_Example_2_Shopping_Cart_System/Refactored_Code/Cart.java
@@ -1,6 +1,6 @@
-package P4_Behavioral_Design_Patterns.P1_Strategy_Design_Pattern.P2_Java_Example_2_Shopping_Cart_System.Refactored_Code;
+package P4_Behavioral_Design_Patterns.P2_Java_Example_2_Shopping_Cart_System.Refactored_Code;
 
-import P4_Behavioral_Design_Patterns.P1_Strategy_Design_Pattern.P2_Java_Example_2_Shopping_Cart_System.Refactored_Code.strategies.ICartStrategy;
+import P4_Behavioral_Design_Patterns.P2_Java_Example_2_Shopping_Cart_System.Refactored_Code.strategies.ICartStrategy;
 
 public class Cart {
     ICartStrategy strategy = null;
---
+++ P4_Behavioral_Design_Patterns/P2_Java_Example_2_Shopping_Cart_System/Refactored_Code/strategies/impl/FlatDiscountCartStrategy.java
@@ -0,0 +1,10 @@
+package P4_Behavioral_Design_Patterns.P2_Java_Example_2_Shopping_Cart_System.Refactored_Code.strategies.impl;
+
+import P4_Behavioral_Design_Patterns.P2_Java_Example_2_Shopping_Cart_System.Refactored_Code.strategies.ICartStrategy;
+
+public class FlatDiscountCartStrategy implements ICartStrategy {
+    @Override
+    public double getPriceAfterDiscount(double originalPrice, double discountPerc) {
+        return originalPrice - (originalPrice * (discountPerc / 100));
+    }
+}
